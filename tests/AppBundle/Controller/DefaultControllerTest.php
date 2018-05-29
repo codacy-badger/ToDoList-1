976fb501ad261ccb305d33a274e7e37e48452f7a
gaeldelmer <gael.delmer.pro@gmail.com>
2018-05-29 19:02:20 +0200

    Test the homepage for logged user

--- tests/AppBundle/Controller/DefaultControllerTest.php
+++ tests/AppBundle/Controller/DefaultControllerTest.php
@@ -3,6 +3,8 @@
 namespace Tests\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 
 class DefaultControllerTest extends WebTestCase
 {
@@ -13,11 +15,35 @@ class DefaultControllerTest extends WebTestCase
         $this->client = static::createClient();
     }
 
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+        $firewallName = 'main';
+
+        $token = new UsernamePasswordToken('user', 'user', $firewallName, array('ROLE_USER'));
+        $session->set('_security_' . $firewallName, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+
+    }
+
     public function testHomepageAnonymous()
     {
         $this->client->request('GET', '/');
 
         $statusCode = $this->client->getResponse()->getStatusCode();
         $this->assertEquals(302, $statusCode);
+    }
+
+    public function testHomepageLogged()
+    {
+        $this->logIn();
+
+        $this->client->request('GET', '/');
+
+        $statusCode = $this->client->getResponse()->getStatusCode();
+        $this->assertEquals(200, $statusCode);
     }
 }
