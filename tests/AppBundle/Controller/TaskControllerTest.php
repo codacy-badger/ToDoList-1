241fcef907baaf45a809051f8bffd834545519f5
gaeldelmer <gael.delmer.pro@gmail.com>
2018-06-07 13:26:51 +0200

    Add a createTask function and delete the created task in testCerateAction

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -2,15 +2,20 @@
 
 namespace Tests\AppBundle\Controller;
 
+use AppBundle\Entity\Task;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
 use Symfony\Component\BrowserKit\Cookie;
 
 class TaskControllerTest extends WebTestCase
 {
+    private $client;
+    private $em;
+
     public function setUp()
     {
         $this->client = static::createClient();
+        $this->em = static::$kernel->getContainer()->get('doctrine')->getManager();
     }
 
     public function login()
@@ -24,6 +29,18 @@ class TaskControllerTest extends WebTestCase
 
         $cookie = new Cookie($session->getName(), $session->getId());
         $this->client->getCookieJar()->set($cookie);
+    }
+
+    public function createTask()
+    {
+        $this->login();
+
+        $task = new Task();
+        $task->setTitle('titleTest');
+        $task->setContent('contentTest');
+        $task->setAuthor($this->client);
+        $this->em->persist($task);
+        $this->em->flush();
     }
 
     public function testListAction()
@@ -53,5 +70,10 @@ class TaskControllerTest extends WebTestCase
         $statusCode = $this->client->getResponse()->getStatusCode();
         $this->assertSame(200, $statusCode);
         $this->assertSame(1, $crawler->filter('div.alert.alert-success')->count());
+
+        $task = $this->em->getRepository('AppBundle:Task')
+            ->findOneBy(['title' => 'testTitle']);
+        $this->em->remove($task);
+        $this->em->flush();
     }
 }
